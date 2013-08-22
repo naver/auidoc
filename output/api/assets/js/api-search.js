411d89cad0b2dd07f2b5408bcb99b16907883dbc
hooriza <hooriza@gmail.com>
2013-08-22 17:58:11 +0900

    코드 오류 수정

--- output/api/assets/js/api-search.js
+++ output/api/assets/js/api-search.js
@@ -1,98 +1,176 @@
-YUI.add('api-search', function (Y) {
+YUI.add('api-search', function(Y) {
+    
+    var tabname = null;
 
-var Lang   = Y.Lang,
-    Node   = Y.Node,
-    YArray = Y.Array;
+    Y.APISearch = function() {};
+    
+    function toSingular(p) {
+        
+        var s = p.replace(/s$/, ''); 
+        
+        return {
+            'classe' : 'class',
+            'propertie' : 'property',
+            'attr' : 'attribute'            
+        }[s] || s;
+        
+    };
+    
+    function toPlural(s) {
+        
+        var p = s + 's'; 
+        
+        return {
+            'classs' : 'classes',
+            'propertys' : 'properties',
+            'attributes' : 'attrs'            
+        }[p] || p;
+        
+    };
+    
+    Y.APISearch.prototype.changeTab = function(name) {
+        tabname = name;
+        refreshResults();
+    };
 
-Y.APISearch = Y.Base.create('apiSearch', Y.Base, [Y.AutoCompleteBase], {
-    // -- Public Properties ----------------------------------------------------
-    RESULT_TEMPLATE:
-        '<li class="result {resultType}">' +
-            '<a href="{url}">' +
-                '<h3 class="title">{name}</h3>' +
-                '<span class="type">{resultType}</span>' +
-                '<div class="description">{description}</div>' +
-                '<span class="className">{class}</span>' +
-            '</a>' +
-        '</li>',
-
-    // -- Initializer ----------------------------------------------------------
-    initializer: function () {
-        this._bindUIACBase();
-        this._syncUIACBase();
-    },
-
-    // -- Protected Methods ----------------------------------------------------
-    _apiResultFilter: function (query, results) {
-        // Filter components out of the results.
-        return YArray.filter(results, function (result) {
-            return result.raw.resultType === 'component' ? false : result;
+    var Search      = Y.namespace('Search'),
+        inputNode   = Y.one('#api-filter'),
+        resultNode  = {
+            keywords : Y.one('#api-keywords'),
+            classes : Y.one('#api-classes'),
+            methods : Y.one('#api-methods'),
+            properties : Y.one('#api-properties'),
+            attrs : Y.one('#api-attrs'),
+            events : Y.one('#api-events')
+        },
+        searchingNode = Y.one('#api-tabview'),
+        notSearchingNode = Y.one('#no-search');
+    
+    var data = Y.YUIDoc;
+    var MAXIMUM = 15;
+    
+    searchingNode.hide();
+    
+    function highlightMatch(sStr, sNeedle) {
+        
+        var bMatch = false;
+        var sRegNeedle = sNeedle.replace(/([\?\.\*\+\-\/\(\)\{\}\[\]\:\!\^\$\\\|])/g, "\\$1");
+        
+        var sRet = sStr.replace(new RegExp(sRegNeedle, 'i'), function(_) {
+            bMatch = true;
+            return '<strong>' + _ + '</strong>';
         });
-    },
-
-    _apiResultFormatter: function (query, results) {
-        return YArray.map(results, function (result) {
-            var raw  = Y.merge(result.raw), // create a copy
-                desc = raw.description || '';
-
-            // Convert description to text and truncate it if necessary.
-            desc = Node.create('<div>' + desc + '</div>').get('text');
-
-            if (desc.length > 65) {
-                desc = Y.Escape.html(desc.substr(0, 65)) + ' &hellip;';
-            } else {
-                desc = Y.Escape.html(desc);
+        
+        return bMatch ? sRet : null;
+        
+    }    
+    
+    function getResults(data, field, value) {
+        
+        var result = null;
+        if (!value) { return result; }
+        
+        var lists = data.detail[field] || [];
+        for (var i = 0, len = lists.length; i < len; i++) {
+            
+            var item = lists[i];
+            var matchName = highlightMatch(item.name, value);
+            
+            if (!matchName) { continue; }
+            
+            item.matchName = matchName;
+            
+            result = result || [];
+            result.push(item);
+            
+            if (result.length === MAXIMUM) {
+                break;
             }
-
-            raw['class'] || (raw['class'] = '');
-            raw.description = desc;
-
-            // Use the highlighted result name.
-            raw.name = result.highlighted;
-
-            return Lang.sub(this.RESULT_TEMPLATE, raw);
-        }, this);
-    },
-
-    _apiTextLocator: function (result) {
-        return result.displayName || result.name;
-    }
-}, {
-    // -- Attributes -----------------------------------------------------------
-    ATTRS: {
-        resultFormatter: {
-            valueFn: function () {
-                return this._apiResultFormatter;
-            }
-        },
-
-        resultFilters: {
-            valueFn: function () {
-                return this._apiResultFilter;
-            }
-        },
-
-        resultHighlighter: {
-            value: 'phraseMatch'
-        },
-
-        resultListLocator: {
-            value: 'data.results'
-        },
-
-        resultTextLocator: {
-            valueFn: function () {
-                return this._apiTextLocator;
-            }
-        },
-
-        source: {
-            value: '/api/v1/search?q={query}&count={maxResults}'
+            
         }
+        
+        return result;
+        
     }
-});
+    
+    var memberAnchor = function(item, itemtype) {
+        return projectRoot + 'classes/' + item['class'] + '.html#' + toSingular(itemtype) + '_' + item.name
+    };
+    
+    var memberDetail = function(item, itemtype) {
+        return [
+            item.description ? '<div class="description">' + item.description.replace(/<[^>]+>/g, '') + '</div>' : '',
+            '<span class="className">' + item['class'] + '</span>'
+        ].join('');
+    };
+    
+    var typeAnchors = {
+        'keywords' : function(item) { return projectRoot + 'keywords/' + item.name + '.html'; },
+        'classes' : function(item) { return projectRoot + 'classes/' + item.name + '.html'; },
+        'modules' : function(item) { return projectRoot + 'modules/' + item.name + '.html'; },
+        'methods' : memberAnchor,
+        'properties' : memberAnchor,
+        'attrs' : memberAnchor,
+        'events' : memberAnchor        
+    };
 
-}, '3.4.0', {requires: [
-    'autocomplete-base', 'autocomplete-highlighters', 'autocomplete-sources',
-    'escape'
-]});
+    var typeDetails = {
+        'keywords' : function(item) {
+            var html = [ '<ul>' ];
+            for (var i = 0, len = item['class'].length; i < len; i++) {
+                html.push('<li>' + item['class'][i] + '</li>');   
+            }
+            html.push('</ul>');
+            return html.join('');  
+        },
+        'classes' : function(item) { return item.description ? '<div class="description">' + item.description.replace(/<[^>]+>/g, '') + '</div>' : ''; },
+        'modules' : function(item) { return ''; },
+        'methods' : memberDetail,
+        'properties' : memberDetail,
+        'attrs' : memberDetail,
+        'events' : memberDetail
+    };
+    
+    function refreshResults() {
+        
+        var value = inputNode._node.value;
+        if (value) {
+            searchingNode.show();
+            notSearchingNode.hide();
+        } else {
+            searchingNode.hide();
+            notSearchingNode.show();
+        }
+        
+        var html = [];
+
+        var results = getResults(data, tabname, value) || [];
+        for (var j = 0, cnt = results.length; j < cnt; j++) {
+            
+            var item = results[j];
+            
+            html.push([
+                '<li class="result">',
+                    '<a href="' + typeAnchors[tabname](item, tabname) + '">',
+                        '<h3 class="title">' + item.matchName + '</h3>',
+                        '<span class="type">' + toSingular(tabname) + '</span>',
+                        typeDetails[tabname](item, tabname),
+                    '</a>',
+                '</li>'
+            ].join(''));
+        }
+        
+        if (!results.length) {
+            html.push('<li class="no-result">No results found.</li>');   
+        }
+        
+        resultNode[tabname].setHTML(html.join(''));        
+        
+    }
+    
+    inputNode.on('keyup', refreshResults);
+    inputNode.on('click', refreshResults);
+        
+}, '0.0.1', {
+    requires : [ ]
+});
